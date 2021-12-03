9f6b882f8cc2238f44f880cf255f9fb22d884d31
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-12-03 18:42:18 +0530

    better day 3

--- 3.hs
+++ 3.hs
@@ -1,31 +1,31 @@
-import Data.List (group, maximumBy, minimumBy, sort, transpose)
-import Data.Ord (comparing)
+import Data.List (transpose)
 
 main :: IO ()
 main = do
-  xs <- lines <$> getContents
+  xs <- map (map (== '1')) . lines <$> getContents
   print $ p1 xs
   print $ p2 xs
 
-p1 :: [String] -> Int
+p1 :: [[Bool]] -> Int
 p1 xs = go mostFrequent * go leastFrequent
   where
     go f = toInt . map f $ transpose xs
 
-p2 :: [String] -> Int
+p2 :: [[Bool]] -> Int
 p2 xs = go mostFrequent * go leastFrequent
   where
     go f = toInt . head $ foldl go2 xs [0 .. length (head xs) - 1]
       where
+        go2 [x] _ = [x]
         go2 xs i = do
           let x = f $ map (!! i) xs
           filter ((== x) . (!! i)) xs
 
-mostFrequent :: String -> Char
-mostFrequent = head . maximumBy (comparing length) . group . sort
+mostFrequent :: [Bool] -> Bool
+mostFrequent xs = length (filter id xs) >= length (filter not xs)
 
-leastFrequent :: String -> Char
-leastFrequent = head . minimumBy (comparing length) . group . sort
+leastFrequent :: [Bool] -> Bool
+leastFrequent = not . mostFrequent
 
-toInt :: String -> Int
-toInt = foldl (\acc x -> acc * 2 + read [x]) 0
+toInt :: [Bool] -> Int
+toInt = foldl (\acc x -> acc * 2 + if x then 1 else 0) 0
