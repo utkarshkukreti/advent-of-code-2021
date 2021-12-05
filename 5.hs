965c8410bf8d5798b47e8172154421c9c638d271
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-12-05 12:11:07 +0530

    day 5

---
+++ 5.hs
@@ -0,0 +1,17 @@
+import Data.Char (isDigit)
+import Data.List (group, sort)
+
+main :: IO ()
+main = do
+  xs <- map ((\[a, b, c, d] -> (a, b, c, d)) . (map read . words . map (\c -> if isDigit c then c else ' '))) . lines <$> getContents
+  let xs' = filter (\(x1, y1, x2, y2) -> x1 == x2 || y1 == y2) xs
+  print $ count xs'
+  print $ count xs
+
+count :: [(Int, Int, Int, Int)] -> Int
+count = length . filter (>= 2) . map length . group . sort . concatMap go
+  where
+    go (x1, y1, x2, y2) = do
+      let dx = signum $ x2 - x1
+      let dy = signum $ y2 - y1
+      zip [x1, x1 + dx .. x2] [y1, y1 + dy .. y2]
