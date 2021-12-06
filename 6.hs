13c2f3d5628a6bd0787969db2168926ba35082ff
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-12-06 12:04:09 +0530

    day 6

---
+++ 6.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TupleSections #-}
+
+import qualified Data.IntMap as M
+
+main :: IO ()
+main = do
+  xs <- map read . words . map (\c -> if c == ',' then ' ' else c) <$> getContents
+  print $ run xs !! 80
+  print $ run xs !! 256
+
+run :: [Int] -> [Int]
+run =
+  map (sum . M.elems)
+    . iterate (M.fromListWith (+) . concatMap go . M.toList)
+    . M.fromListWith (+)
+    . map (,1)
+  where
+    go (0, v) = [(6, v), (8, v)]
+    go (k, v) = [(k - 1, v)]
